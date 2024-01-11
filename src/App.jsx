c5b81a2946fc034bc2d2f4f00739691debf1e3bc
“Adeena-Faizzad” <130522128+Adeena-Faizzad@users.noreply.github.com>
2024-01-11 17:18:56 +0300

    Update UI

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ function App() {
       }
       return{
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject ]
       }
     })
@@ -43,7 +44,9 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject}
+      projects={projectsState.projects} 
+       />
       { content }
     </main>
   );
--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Button from './Button.jsx'
 
-function ProjectsSidebar({onStartAddProject}) {
+function ProjectsSidebar({onStartAddProject, projects}) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
         <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Projects</h2>
@@ -10,8 +10,10 @@ function ProjectsSidebar({onStartAddProject}) {
                 + Add Project
             </Button>
         </div>
-        <ul>
-            list of projects
+        <ul className="mt-8">
+            {projects.map(project => <li key={project.id}>
+              <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">{project.title}</button>
+            </li>)}
         </ul>
     </aside>
   )
